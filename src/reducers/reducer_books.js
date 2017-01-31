4b7d0796e7f4a8ae3b7f61984a0dc6614ebc9fdb
Marc Fournier <marc.fournier91@gmail.com>
2017-01-31 15:28:51 -0500

    App complete.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'understanding javascript' },
-    { title: 'harry plopper'},
-    { title: 'Dark Tower'},
-    { title: 'eloquent ruby'},
+    { title: 'understanding javascript', pages: 85 },
+    { title: 'harry plopper', pages: 64},
+    { title: 'Dark Tower', pages: 42},
+    { title: 'eloquent ruby', pages: 2},
   ]
 }
