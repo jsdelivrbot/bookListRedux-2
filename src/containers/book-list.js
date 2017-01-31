472493c788e8e587485fe02465288494cb318a13
Marc Fournier <marc.fournier91@gmail.com>
2017-01-31 13:42:49 -0500

    clicking a book fires a pretend action creator.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book,key) => {
       return (
-        <li key={book.key} className="list-group-item">{book.title}</li>
+        <li key={key} onClick={() => this.props.selectBook(book)} className="list-group-item">{book.title}</li>
       )
     })
   }
@@ -26,4 +28,8 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
